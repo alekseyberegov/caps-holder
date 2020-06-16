c32f98b2dff00b43e57b874b706381be3f27e170
clicktripz <l87dg5f1!>
2020-06-15 19:36:51 -0700

    Updated dockerfile

--- caps-holder/capsholder/stubs/cats/Stub.py
+++ caps-holder/capsholder/stubs/cats/Stub.py
@@ -6,17 +6,18 @@ from urllib3.exceptions import InsecureRequestWarning
 
 
 class Stub(object):
-    def __init__(self, endpoint):
+    def __init__(self, endpoint, debug=False):
         self.endpoint = endpoint
+        self.debug = debug
         requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
     def send(self, url):
-        req = {'url': url, 'debug': True}
+        req = {'url': url, 'debug': self.debug}
 
         try:
             res = requests.post(self.endpoint, json=req, timeout=1, verify=False)
             payload = res.content.decode('utf-8')
-        except requests.exceptions.SSLError as error:
+        except (ConnectionError, requests.RequestException):
             payload = self.create_error_response()
 
         return payload
@@ -25,5 +26,5 @@ class Stub(object):
     def create_error_response():
         exc_type, exc_value, exc_traceback = sys.exc_info()
         res: str = repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
-        res = '{"status": "error", "stacktrace" : "%s"}' % urllib.parse.quote(res)
+        res = '{"status": "error", "search": {"query": null}, "stacktrace" : "%s"}' % urllib.parse.quote(res)
         return res
