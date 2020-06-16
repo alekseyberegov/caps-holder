b09c0d5f1120a113e8d432b21c972bd87631484a
clicktripz <l87dg5f1!>
2020-06-16 00:14:20 -0700

    Added parameters to runit script

--- caps_stats.sql
+++ caps_stats.sql
@@ -1,7 +1,7 @@
 .mode column
 .headers on
 .width 40 8
-select search
+select coalesce(search,"<DS WTF>") as search_query
     , count(1) as cnt
     , round(100.0 * count(1) / sum(count(1)) over(), 2) as pct
 from caps_holder
